9e53430f7fc95b6bffed30ddb891972d4138a131
luiso <awdcgy@hanyang.ac.kr>
2023-03-28 21:45:29 +0900

    init commit

---
+++ src/test/java/com/semoss/agricola/AgricolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semoss.agricola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgricolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

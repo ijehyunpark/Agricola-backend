21e5eea2e5376751c46ef63dc3cc5562629b905f
SonChanhyuk <thscksgur0903@gmail.com>
2023-05-08 01:51:45 +0900

    설비카드 사용제외 작성

---
+++ src/test/java/com/semoss/agricola/GamePlay/domain/action/BakeActionTest.java
@@ -0,0 +1,32 @@
+package com.semoss.agricola.GamePlay.domain.action;
+
+import com.semoss.agricola.GamePlay.domain.Player;
+import com.semoss.agricola.GamePlay.domain.ResourceType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BakeActionTest {
+    Player player;
+    BakeAction bakeAction;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player("test");
+        bakeAction = new BakeAction(2);
+    }
+
+    @Test
+    void runAction() {
+        assertFalse(bakeAction.checkPrecondition(player));
+        player.addResource(ResourceType.GRAIN,2);
+        assertTrue(bakeAction.checkPrecondition(player));
+
+        bakeAction.runAction(player);
+        assertEquals(2,player.getResource(ResourceType.FOOD));
+
+        bakeAction.runAction(player);
+        assertFalse(bakeAction.checkPrecondition(player));
+    }
+}

26f61ae259dd67d44da7fcfdf665af9a152c3194
luiso <awdcgy@hanyang.ac.kr>
2023-06-07 18:41:58 +0900

    더미 직업 카드 및 더미 보조설비 생성 및 배치

--- src/main/java/com/semoss/agricola/GamePlay/domain/card/CardDictionary.java
+++ src/main/java/com/semoss/agricola/GamePlay/domain/card/CardDictionary.java
@@ -3,8 +3,14 @@ package com.semoss.agricola.GamePlay.domain.card;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.semoss.agricola.GamePlay.domain.card.Majorcard.MajorCard;
+import com.semoss.agricola.GamePlay.domain.card.Minorcard.DummyMinorCard;
+import com.semoss.agricola.GamePlay.domain.card.Minorcard.MinorCard;
+import com.semoss.agricola.GamePlay.domain.card.Occupation.DummyOccupation;
+import com.semoss.agricola.GamePlay.domain.card.Occupation.Occupation;
 import com.semoss.agricola.GamePlay.domain.player.Player;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -18,18 +24,40 @@ import java.util.stream.Collectors;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Getter
+@Log4j2
 public class CardDictionary {
     @JsonIgnore
     public final Map<Long, Card> cardDict = new HashMap<>();
     public final Map<Long, Player> ownerDict = new HashMap<>();
 
-    public CardDictionary(List<MajorCard> majorCards){
+    public CardDictionary(List<MajorCard> majorCards,
+                          List<MinorCard> minorCards, ObjectProvider<DummyMinorCard> dummyMinorCards,
+                          List<Occupation> occupations, ObjectProvider<DummyOccupation> dummyOccupations){
+        log.debug("card 사전 생성: " + this.hashCode());
         // 주설비 카드 등록
         majorCards.forEach(majorCard -> cardDict.put(majorCard.getCardID(), majorCard));
 
-        // TODO: 보조 설비 카드 등록
+        // 보조 설비 카드 등록
+        int minorCardSize = minorCards.size();
+        log.debug("보조 설비 카드 개수: " + minorCardSize);
+        minorCards.forEach(minorCard -> cardDict.put(minorCard.getCardID(), minorCard));
 
-        // TODO: 직업 카드 등록
+        //보조 설비 더미 카드 생성
+        for (int i = minorCardSize; i < 28; i++) {
+            DummyMinorCard dummyMinorCard = dummyMinorCards.getObject();
+            cardDict.put(dummyMinorCard.getCardID(), dummyMinorCard);
+        }
+
+        // 직업 카드 등록
+        int occupationSize = occupations.size();
+        log.debug("직업 카드 개수: " + occupationSize);
+        occupations.forEach(occupation -> cardDict.put(occupation.getCardID(), occupation));
+
+        //직업카드 더미 카드 생성
+        for (int i = occupationSize; i < 28; i++) {
+            DummyOccupation dummyOccupation = dummyOccupations.getObject();
+            cardDict.put(dummyOccupation.getCardID(), dummyOccupation);
+        }
 
     }
 
--- src/main/java/com/semoss/agricola/GamePlay/domain/card/Minorcard/DummyMinorCard.java
+++ src/main/java/com/semoss/agricola/GamePlay/domain/card/Minorcard/DummyMinorCard.java
@@ -1,23 +1,29 @@
 package com.semoss.agricola.GamePlay.domain.card.Minorcard;
 
-import com.semoss.agricola.GamePlay.domain.card.CardType;
 import com.semoss.agricola.GamePlay.domain.player.Player;
 import com.semoss.agricola.GamePlay.domain.resource.ResourceStruct;
 import com.semoss.agricola.GamePlay.domain.resource.ResourceType;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * 더미 보조설비 카드 입니다.
+ * 구걸카드 1장 획득
  */
 @Getter
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Log4j2
 public class DummyMinorCard extends DefaultMinorCard implements PlaceGetResourceTrigger{
     private final ResourceStruct bonusResource = ResourceStruct.builder().resource(ResourceType.BEGGING).count(1).build();
     private static Long nextID = 1001L;
 
-    @Builder
-    protected DummyMinorCard(int bonusPoint, ResourceStruct[] ingredients, CardType preconditionCardType, int minCardNum, String name, String description) {
-        super(nextID, bonusPoint, ingredients, preconditionCardType, minCardNum, name, description);
+    public DummyMinorCard() {
+        super(nextID, 0, new ResourceStruct[]{}, null, 0, "더미 보조설비", "구걸토큰 1개를 획득합니다.");
+        log.debug("더미 카드 생성 :" + this.hashCode());
         nextID += 2;
     }
 
